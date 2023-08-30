20883e98cb07fc3a870af6e19c760cd214106430
mikor85 <cormakoff@gmail.com>
2023-08-30 21:57:06 +0300

    Added exercises.

--- src/main/java/Medium/Reverse_Integer_7/ReverseInteger.java
+++ src/main/java/Medium/Reverse_Integer_7/ReverseInteger.java
@@ -2,7 +2,7 @@ package Medium.Reverse_Integer_7;
 
 /*
  * Given a signed 32-bit integer x, return x with its digits reversed.
- * If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ * If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
  *
  * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
  *
@@ -50,5 +50,26 @@ public class ReverseInteger {
         }
 
         return 0;
+    }
+
+    public static int reverseVersion2(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int pop = x % 10;
+
+            // Check for integer overflow before updating reversed
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + pop;
+            x /= 10;
+        }
+
+        return reversed;
     }
 }
--- src/test/java/Medium/Reverse_Integer_7/ReverseIntegerTest.java
+++ src/test/java/Medium/Reverse_Integer_7/ReverseIntegerTest.java
@@ -11,11 +11,14 @@ public class ReverseIntegerTest {
 
         int x1 = 123;
         assertEquals(321, ReverseInteger.reverse(x1));
+        assertEquals(321, ReverseInteger.reverseVersion2(x1));
 
         int x2 = -123;
         assertEquals(-321, ReverseInteger.reverse(x2));
+        assertEquals(-321, ReverseInteger.reverseVersion2(x2));
 
         int x3 = 120;
         assertEquals(21, ReverseInteger.reverse(x3));
+        assertEquals(21, ReverseInteger.reverseVersion2(x3));
     }
 }
